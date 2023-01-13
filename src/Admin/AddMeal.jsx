a2c0fe426fe1a429434b5a967ac35f482027360c
hamsa <hafieekhan@gmail.com>
2023-01-13 09:17:01 +0300

    "Update"

---
+++ src/Admin/AddMeal.jsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import { useNavigate } from "react-router-dom";
+
+
+function AddMeal() {
+    const [img, setImage] = useState("");
+    const [content, setContent] = useState("");
+    const [category, setCategory] = useState("");
+    const [author, setAuthor] = useState("");
+    const [isPending, setisPending] = useState(false);
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const article = { title, content, author, category };
+      console.log(article);
+      setisPending(true);
+
+      fetch("http://localhost:9292/posts", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(article),
+      })
+        .then((res) => res.json())
+        .then(() => {
+          setAuthor("");
+          setContent("");
+          setTitle("");
+          setCategory("");
+          setisPending(false);
+          navigate("/");
+        });
+    };
+
+    return (
+      <div className="p-8">
+        <h2 className="text-3xl text-gray-600 font-bold">Add a New Post</h2>
+        <form className="p-4 flex flex-col" onSubmit={handleSubmit}>
+          <label className="pb-4 mt-4">Article Title:</label>
+          <input
+            className="p-2 border border-solid border-gray-300"
+            type="text"
+            required
+            value={title}
+            placeholder="Add a title"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <label className="pb-4 mt-4">Article Author:</label>
+          <input
+            className="p-2 border border-solid border-gray-300 "
+            type="text"
+            required
+            value={author}
+            placeholder="Add your name"
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+          <label className="pb-4 mt-4">Article Category:</label>
+          <input
+            className="p-2 border border-solid border-gray-300"
+            type="text"
+            required
+            value={category}
+            placeholder="Add a category"
+            onChange={(e) => setCategory(e.target.value)}
+          />
+          {/* <label className='pb-4 mt-4'>Article Author:</label>
+                  <input className='p-2 border border-solid border-gray-300 '
+                  type="text"
+                  required
+                  value={author}
+                  placeholder="Add your name"
+                  onChange={(e)=>setAuthor(e.target.value)}
+              /> */}
+          <label className="mt-4 pb-4">Article Body: </label>
+          <textarea
+            className="py-10 border border-solid border-gray-300"
+            required
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+
+          {!isPending && (
+            <button className="mx-80 p-2 mt-6 border border-gray-400 rounded-full text-gray-600">
+              Add Blog
+            </button>
+          )}
+          {isPending && <button disabled>Adding Blog</button>}
+        </form>
+      </div>
+    );
+  }
+
+export default AddMeal
--- src/Admin/Dashboard.jsx
+++ src/Admin/Dashboard.jsx
@@ -39,18 +39,19 @@ function Dashboard() {
         <div class="p-5">
           <a href="#">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
+              Piau & rice
             </h5>
+            <h2>10$</h2>
           </a>
           <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
+          Rice pilau is a fragrant tasty one-pot dish made with caramelized beef with rice and potato cooked in a rich and delicious meat or chicken
           </p>
+          <div className="flex space-x-4 ">
           <a
             href="#"
             class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            Read more
+            Add Meal
             <svg
               aria-hidden="true"
               class="w-4 h-4 ml-2 -mr-1"
@@ -65,6 +66,45 @@ function Dashboard() {
               ></path>
             </svg>
           </a>
+          <a
+            href="#"
+            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Delete
+            <svg
+              aria-hidden="true"
+              class="w-4 h-4 ml-2 -mr-1"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </a>
+          <a
+            href="#"
+            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Modify
+            <svg
+              aria-hidden="true"
+              class="w-4 h-4 ml-2 -mr-1"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </a>
+          </div>
         </div>
       </div>
 
